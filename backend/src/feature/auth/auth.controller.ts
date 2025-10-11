40136c5dfb37b2b8a4a4a751ce632889e1d1d52e
Carbon2301 <trinhhuuan372@gmail.com>
2025-10-11 09:15:00 +0700

    Phát triển module Authentication

---
+++ backend/src/feature/auth/auth.controller.ts
@@ -0,0 +1,58 @@
+import { Body, Controller, Get, Post, Query, Res } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { GetAuthorizationUrlResDTO, LoginBodyDTO, LoginResDTO, LogoutBodyDTO, LogoutResDTO, RegisterBodyDTO, RegisterResDTO } from './auth.dto'
+import envConfig from '../../shared/config'
+import { Response } from 'express'
+import { GoogleService } from './google.service'
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly googleService: GoogleService
+  ) { }
+
+  @Post('register')
+  async register(@Body() body: RegisterBodyDTO) {
+    return new RegisterResDTO(await this.authService.register(body))
+  }
+
+  @Post('login')
+  async login(@Body() body: LoginBodyDTO) {
+    return new LoginResDTO(await this.authService.login(body))
+  }
+
+  @Post('logout')
+  async logout(@Body() body: LogoutBodyDTO) {
+    return new LogoutResDTO(await this.authService.logout(body.refreshToken))
+  }
+
+  @Get('google-link')
+  getAuthorizationUrl() {
+    return new GetAuthorizationUrlResDTO(this.googleService.getAuthorizationUrl())
+  }
+
+  @Get('google/callback')
+  async googleCallback(
+    @Query('code') code: string,
+    @Query('state') state: string,
+    @Res() res: Response,
+  ) {
+    try {
+      const data = await this.googleService.googleCallback({ code, state })
+
+      return res.redirect(
+        `${envConfig.GOOGLE_CLIENT_REDIRECT_URL}?accessToken=${data.accessToken}&refreshToken=${data.refreshToken}&user=${encodeURIComponent(JSON.stringify(data.user))}`,
+      )
+    } catch (error) {
+      const message =
+        error instanceof Error
+          ? error.message
+          : 'Đã xảy ra lỗi khi đăng nhập bằng Google, vui lòng thử lại.'
+
+      return res.redirect(
+        `${envConfig.GOOGLE_CLIENT_REDIRECT_URL}?errorMessage=${encodeURIComponent(message)}`,
+      )
+    }
+  }
+}
---
+++ backend/src/feature/auth/auth.dto.ts
@@ -0,0 +1,78 @@
+import { IsEmail, IsNotEmpty, IsString, IsUrl, Length, Matches } from 'class-validator'
+import { Match } from '../../shared/decorators/custom-validation.decorator'
+import { Exclude } from 'class-transformer'
+
+export class LoginBodyDTO {
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail({}, { message: 'Email không hợp lệ' })
+  email: string
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(6, 20, { message: 'Mật khẩu phải từ 6 đến 20 kí tự' })
+  password: string
+}
+
+export class LoginResDTO {
+  accessToken: string
+  refreshToken: string
+  user: {
+    name: string
+    email: string
+  }
+  constructor(partial: Partial<LoginResDTO>) {
+    Object.assign(this, partial)
+  }
+}
+
+export class RegisterBodyDTO extends LoginBodyDTO {
+  @IsString({ message: 'Tên phải là chuỗi' })
+  @IsNotEmpty()
+  name: string
+
+  @IsString()
+  @IsNotEmpty()
+  @Match('password', { message: 'Mật khẩu và xác nhận mật khẩu không khớp nhau!' })
+  confirmPassword: string
+}
+
+export class RegisterResDTO {
+  id: number
+  email: string
+  name: string
+  @Exclude() password: string
+  createdAt: Date
+  updatedAt: Date
+
+  constructor(partial: Partial<RegisterResDTO>) {
+    Object.assign(this, partial)
+  }
+}
+
+export class RefreshTokenBodyDTO {
+  @IsString()
+  refreshToken: string
+}
+
+export class RefreshTokenResDTO extends LoginResDTO {}
+
+export class LogoutBodyDTO extends RefreshTokenBodyDTO {}
+
+export class LogoutResDTO {
+  message: string
+  constructor(partial: Partial<LogoutResDTO>) {
+    Object.assign(this, partial)
+  }
+}
+
+export class GetAuthorizationUrlResDTO {
+  @IsString()
+  @IsNotEmpty()
+  @IsUrl()
+  url: string
+
+  constructor(partial: Partial<GetAuthorizationUrlResDTO>) {
+    Object.assign(this, partial)
+  }
+}
