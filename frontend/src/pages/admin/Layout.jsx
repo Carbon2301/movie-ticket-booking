d94ff1b33fe04bc67ed4487d542b8a40a8e8d1d1
kiwwi1 <dquanghuy221@gmail.com>
2025-10-31 09:20:00 +0700

    Tạo Admin layout và navigation

---
+++ frontend/src/pages/admin/Layout.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import AdminNavbar from "../../components/admin/AdminNavbar";
+import AdminSidebar from "../../components/admin/AdminSidebar";
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Layout =() => {
+  return (
+    <div>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      <AdminNavbar />
+      <div className="flex">
+        <AdminSidebar />
+        <div className="flex-1 px-4 py-10 md:px-10 h-[calc(100vh-64px)] overflow-y-auto">
+          <Outlet /> 
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
